1c2a4fc9ec853bf343f8dac625a0d8cf4cc531dd
Lukas Klingsbo <me@lukas.fyi>
2022-08-19 20:11:19 +0200

    chore: Release v0.5.0

--- lib/templates/bricks/simple_bundle.dart
+++ lib/templates/bricks/simple_bundle.dart
@@ -3,4 +3,40 @@
 
 import 'package:mason/mason.dart';
 
-final simpleBundle = MasonBundle.fromJson(<String, dynamic>{"files":[{"path":"analysis_options.yaml","data":"aW5jbHVkZTogcGFja2FnZTpmbGFtZV9saW50L2FuYWx5c2lzX29wdGlvbnMueWFtbA==","type":"text"},{"path":"lib/main.dart","data":"aW1wb3J0ICdwYWNrYWdlOmZsYW1lL2dhbWUuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbHV0dGVyL21hdGVyaWFsLmRhcnQnOwoKdm9pZCBtYWluKCkgewogIHJ1bkFwcChHYW1lV2lkZ2V0KGdhbWU6IE15R2FtZSgpKSk7Cn0KCmNsYXNzIE15R2FtZSBleHRlbmRzIEZsYW1lR2FtZSB7CiAgQG92ZXJyaWRlCiAgRnV0dXJlPHZvaWQ+IG9uTG9hZCgpIGFzeW5jIHsKICAgIC8vIFRPRE8gYWRkIHlvdXIgbG9hZCBsb2dpYwogICAgcmV0dXJuIHN1cGVyLm9uTG9hZCgpOwogIH0KfQo=","type":"text"},{"path":"pubspec.yaml","data":"bmFtZToge3tuYW1lfX0KZGVzY3JpcHRpb246IHt7ZGVzY3JpcHRpb259fQp2ZXJzaW9uOiB7e3ZlcnNpb259fQoKcHVibGlzaF90bzogJ25vbmUnCgplbnZpcm9ubWVudDoKICBzZGs6ICI+PTIuMTcuMCA8My4wLjAiCgpkZXBlbmRlbmNpZXM6CiAgZmx1dHRlcjoKICAgIHNkazogZmx1dHRlcnt7I2V4dHJhLWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGVwZW5kZW5jaWVzfX0KCmRldl9kZXBlbmRlbmNpZXM6e3sjZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAgZmx1dHRlcl90ZXN0OgogICAgc2RrOiBmbHV0dGVyCiAgaW50ZWdyYXRpb25fdGVzdDoKICAgIHNkazogZmx1dHRlcgoKZmx1dHRlcjoKICB1c2VzLW1hdGVyaWFsLWRlc2lnbjogZmFsc2U=","type":"text"}],"hooks":[],"name":"simple","description":"An empty Flame project with just the bare minimums to get you up and running.","version":"0.1.0","environment":{"mason":"any"},"vars":{"name":{"type":"string"},"description":{"type":"string"},"version":{"type":"string"},"flame-version":{"type":"string"},"flame-lint-version":{"type":"string"},"extra-dependencies":{"type":"array"},"extra-dev-dependencies":{"type":"array"}}});
+final simpleBundle = MasonBundle.fromJson(<String, dynamic>{
+  "files": [
+    {
+      "path": "analysis_options.yaml",
+      "data":
+          "aW5jbHVkZTogcGFja2FnZTpmbGFtZV9saW50L2FuYWx5c2lzX29wdGlvbnMueWFtbA==",
+      "type": "text"
+    },
+    {
+      "path": "lib/main.dart",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOmZsYW1lL2dhbWUuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbHV0dGVyL21hdGVyaWFsLmRhcnQnOwoKdm9pZCBtYWluKCkgewogIHJ1bkFwcChHYW1lV2lkZ2V0KGdhbWU6IE15R2FtZSgpKSk7Cn0KCmNsYXNzIE15R2FtZSBleHRlbmRzIEZsYW1lR2FtZSB7CiAgQG92ZXJyaWRlCiAgRnV0dXJlPHZvaWQ+IG9uTG9hZCgpIGFzeW5jIHsKICAgIC8vIFRPRE8gYWRkIHlvdXIgbG9hZCBsb2dpYwogICAgcmV0dXJuIHN1cGVyLm9uTG9hZCgpOwogIH0KfQo=",
+      "type": "text"
+    },
+    {
+      "path": "pubspec.yaml",
+      "data":
+          "bmFtZToge3tuYW1lfX0KZGVzY3JpcHRpb246IHt7ZGVzY3JpcHRpb259fQp2ZXJzaW9uOiB7e3ZlcnNpb259fQoKcHVibGlzaF90bzogJ25vbmUnCgplbnZpcm9ubWVudDoKICBzZGs6ICI+PTIuMTcuMCA8My4wLjAiCgpkZXBlbmRlbmNpZXM6CiAgZmx1dHRlcjoKICAgIHNkazogZmx1dHRlcnt7I2V4dHJhLWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGVwZW5kZW5jaWVzfX0KCmRldl9kZXBlbmRlbmNpZXM6e3sjZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAgZmx1dHRlcl90ZXN0OgogICAgc2RrOiBmbHV0dGVyCiAgaW50ZWdyYXRpb25fdGVzdDoKICAgIHNkazogZmx1dHRlcgoKZmx1dHRlcjoKICB1c2VzLW1hdGVyaWFsLWRlc2lnbjogZmFsc2U=",
+      "type": "text"
+    }
+  ],
+  "hooks": [],
+  "name": "simple",
+  "description":
+      "An empty Flame project with just the bare minimums to get you up and running.",
+  "version": "0.1.0",
+  "environment": {"mason": "any"},
+  "vars": {
+    "name": {"type": "string"},
+    "description": {"type": "string"},
+    "version": {"type": "string"},
+    "flame-version": {"type": "string"},
+    "flame-lint-version": {"type": "string"},
+    "extra-dependencies": {"type": "array"},
+    "extra-dev-dependencies": {"type": "array"}
+  }
+});
