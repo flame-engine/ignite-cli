46c8052e06b960fded63c3bd9fdbd5b7de5e7269
Luan Nico <luanpotter27@gmail.com>
2023-02-04 16:54:05 -0500

    Untouch mason files

--- lib/templates/bricks/example_bundle.dart
+++ lib/templates/bricks/example_bundle.dart
@@ -1,6 +1,48 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint, implicit_dynamic_list_literal, implicit_dynamic_map_literal, inference_failure_on_collection_literal
+// ignore_for_file: prefer_single_quotes, public_member_api_docs, lines_longer_than_80_chars, implicit_dynamic_list_literal, implicit_dynamic_map_literal
 
 import 'package:mason/mason.dart';
 
-final exampleBundle = MasonBundle.fromJson(<String, dynamic>{"files":[{"path":"analysis_options.yaml","data":"aW5jbHVkZTogcGFja2FnZTpmbGFtZV9saW50L2FuYWx5c2lzX29wdGlvbnMueWFtbA==","type":"text"},{"path":"lib/main.dart","data":"aW1wb3J0ICdkYXJ0Om1hdGgnOwoKaW1wb3J0ICdwYWNrYWdlOmZsYW1lL2NvbXBvbmVudHMuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbGFtZS9nYW1lLmRhcnQnOwppbXBvcnQgJ3BhY2thZ2U6ZmxhbWUvaW5wdXQuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbGFtZS9wYWxldHRlLmRhcnQnOwppbXBvcnQgJ3BhY2thZ2U6Zmx1dHRlci9tYXRlcmlhbC5kYXJ0JzsKCmZpbmFsIF9ybmcgPSBSYW5kb20oKTsKCnZvaWQgbWFpbigpIHsKICBydW5BcHAoR2FtZVdpZGdldChnYW1lOiBNeUdhbWUoKSkpOwp9CgpjbGFzcyBNeUNvbXBvbmVudCBleHRlbmRzIFBvc2l0aW9uQ29tcG9uZW50IHdpdGggSGFzR2FtZVJlZjxNeUdhbWU+IHsKICBzdGF0aWMgZmluYWwgX3BhaW50ID0gQmFzaWNQYWxldHRlLndoaXRlLnBhaW50KCk7CiAgZmluYWwgVmVjdG9yMiBzcGVlZCA9IFZlY3RvcjIuemVybygpOwoKICBAb3ZlcnJpZGUKICBAb3ZlcnJpZGUKICBGdXR1cmU8dm9pZD4gb25Mb2FkKCkgYXN5bmMgewogICAgYW5jaG9yID0gQW5jaG9yLmNlbnRlcjsKICAgIHBvc2l0aW9uID0gZ2FtZVJlZi5zaXplIC8gMjsKICB9CgogIEBvdmVycmlkZQogIHZvaWQgcmVuZGVyKENhbnZhcyBjKSB7CiAgICBjLmRyYXdSZWN0KHNpemUudG9SZWN0KCksIF9wYWludCk7CiAgfQoKICBAb3ZlcnJpZGUKICB2b2lkIHVwZGF0ZShkb3VibGUgZHQpIHsKICAgIHBvc2l0aW9uICs9IHNwZWVkICogZHQ7CiAgfQp9CgpjbGFzcyBNeUdhbWUgZXh0ZW5kcyBGbGFtZUdhbWUgd2l0aCBUYXBEZXRlY3RvciB7CiAgbGF0ZSBmaW5hbCBNeUNvbXBvbmVudCBteUNvbXBvbmVudDsKCiAgQG92ZXJyaWRlCiAgRnV0dXJlPHZvaWQ+IG9uTG9hZCgpIGFzeW5jIHsKICAgIGF3YWl0IGFkZChteUNvbXBvbmVudCA9IE15Q29tcG9uZW50KCkpOwogICAgcmV0dXJuIHN1cGVyLm9uTG9hZCgpOwogIH0KCiAgQG92ZXJyaWRlCiAgdm9pZCBvblRhcCgpIHsKICAgIG15Q29tcG9uZW50LnNwZWVkLnggPSAtNSArIDEwICogX3JuZy5uZXh0RG91YmxlKCk7CiAgICBteUNvbXBvbmVudC5zcGVlZC55ID0gLTUgKyAxMCAqIF9ybmcubmV4dERvdWJsZSgpOwogIH0KfQo=","type":"text"},{"path":"pubspec.yaml","data":"bmFtZToge3tuYW1lfX0KZGVzY3JpcHRpb246IHt7ZGVzY3JpcHRpb259fQp2ZXJzaW9uOiB7e3ZlcnNpb259fQoKcHVibGlzaF90bzogJ25vbmUnCgplbnZpcm9ubWVudDoKICBzZGs6ICI+PTIuMTcuMCA8My4wLjAiCgpkZXBlbmRlbmNpZXM6CiAgZmx1dHRlcjoKICAgIHNkazogZmx1dHRlcnt7I2V4dHJhLWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGVwZW5kZW5jaWVzfX0KCmRldl9kZXBlbmRlbmNpZXM6e3sjZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAgZmx1dHRlcl90ZXN0OgogICAgc2RrOiBmbHV0dGVyCiAgaW50ZWdyYXRpb25fdGVzdDoKICAgIHNkazogZmx1dHRlcgoKZmx1dHRlcjoKICB1c2VzLW1hdGVyaWFsLWRlc2lnbjogZmFsc2U=","type":"text"},{"path":"test/widget_test.dart","data":"aW1wb3J0ICdwYWNrYWdlOmZsYW1lL2dhbWUuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbGFtZV90ZXN0L2ZsYW1lX3Rlc3QuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbHV0dGVyX3Rlc3QvZmx1dHRlcl90ZXN0LmRhcnQnOwoKaW1wb3J0ICdwYWNrYWdlOnt7bmFtZX19L21haW4uZGFydCc7CgpmaW5hbCBteUdhbWUgPSBGbGFtZVRlc3RlcihNeUdhbWUubmV3KTsKCnZvaWQgbWFpbigpIHsKICBteUdhbWUudGVzdEdhbWVXaWRnZXQoCiAgICAnZ2FtZSB3aWxsIGxvYWQgaXRzIGNoaWxkJywKICAgIHZlcmlmeTogKGdhbWUsIHRlc3RlcikgYXN5bmMgewogICAgICBnYW1lLnVwZGF0ZSgwLjApOwoKICAgICAgZXhwZWN0KGdhbWUuY2hpbGRyZW4ubGVuZ3RoLCAxKTsKICAgICAgZXhwZWN0KGdhbWUubXlDb21wb25lbnQuc3BlZWQsIFZlY3RvcjIuemVybygpKTsKCiAgICAgIGF3YWl0IHRlc3Rlci50YXBBdChjb25zdCBPZmZzZXQoMTAsIDEwKSk7CiAgICAgIGV4cGVjdChnYW1lLm15Q29tcG9uZW50LnNwZWVkLCBpc05vdChlcXVhbHMoVmVjdG9yMi56ZXJvKCkpKSk7CiAgICB9LAogICk7Cn0K","type":"text"}],"hooks":[],"name":"example","description":"An actual complete, working game example. Extra code that you won't need but will teach you how to wire the most important pieces.","version":"0.1.0","environment":{"mason":"any"},"vars":{"name":{"type":"string"},"description":{"type":"string"},"version":{"type":"string"},"flame-version":{"type":"string"},"flame-lint-version":{"type":"string"},"extra-dependencies":{"type":"array"},"extra-dev-dependencies":{"type":"array"}}});
+final exampleBundle = MasonBundle.fromJson(<String, dynamic>{
+  "files": [
+    {
+      "path": "analysis_options.yaml",
+      "data":
+          "aW5jbHVkZTogcGFja2FnZTpmbGFtZV9saW50L2FuYWx5c2lzX29wdGlvbnMueWFtbA==",
+      "type": "text"
+    },
+    {
+      "path": "lib/main.dart",
+      "data":
+          "aW1wb3J0ICdkYXJ0Om1hdGgnOwoKaW1wb3J0ICdwYWNrYWdlOmZsYW1lL2NvbXBvbmVudHMuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbGFtZS9nYW1lLmRhcnQnOwppbXBvcnQgJ3BhY2thZ2U6ZmxhbWUvaW5wdXQuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbGFtZS9wYWxldHRlLmRhcnQnOwppbXBvcnQgJ3BhY2thZ2U6Zmx1dHRlci9tYXRlcmlhbC5kYXJ0JzsKCmZpbmFsIF9ybmcgPSBSYW5kb20oKTsKCnZvaWQgbWFpbigpIHsKICBydW5BcHAoR2FtZVdpZGdldChnYW1lOiBNeUdhbWUoKSkpOwp9CgpjbGFzcyBNeUNvbXBvbmVudCBleHRlbmRzIFBvc2l0aW9uQ29tcG9uZW50IHdpdGggSGFzR2FtZVJlZjxNeUdhbWU+IHsKICBzdGF0aWMgZmluYWwgX3BhaW50ID0gQmFzaWNQYWxldHRlLndoaXRlLnBhaW50KCk7CiAgZmluYWwgVmVjdG9yMiBzcGVlZCA9IFZlY3RvcjIuemVybygpOwoKICBAb3ZlcnJpZGUKICBAb3ZlcnJpZGUKICBGdXR1cmU8dm9pZD4gb25Mb2FkKCkgYXN5bmMgewogICAgYW5jaG9yID0gQW5jaG9yLmNlbnRlcjsKICAgIHBvc2l0aW9uID0gZ2FtZVJlZi5zaXplIC8gMjsKICB9CgogIEBvdmVycmlkZQogIHZvaWQgcmVuZGVyKENhbnZhcyBjKSB7CiAgICBjLmRyYXdSZWN0KHNpemUudG9SZWN0KCksIF9wYWludCk7CiAgfQoKICBAb3ZlcnJpZGUKICB2b2lkIHVwZGF0ZShkb3VibGUgZHQpIHsKICAgIHBvc2l0aW9uICs9IHNwZWVkICogZHQ7CiAgfQp9CgpjbGFzcyBNeUdhbWUgZXh0ZW5kcyBGbGFtZUdhbWUgd2l0aCBUYXBEZXRlY3RvciB7CiAgbGF0ZSBmaW5hbCBNeUNvbXBvbmVudCBteUNvbXBvbmVudDsKCiAgQG92ZXJyaWRlCiAgRnV0dXJlPHZvaWQ+IG9uTG9hZCgpIGFzeW5jIHsKICAgIGF3YWl0IGFkZChteUNvbXBvbmVudCA9IE15Q29tcG9uZW50KCkpOwogICAgcmV0dXJuIHN1cGVyLm9uTG9hZCgpOwogIH0KCiAgQG92ZXJyaWRlCiAgdm9pZCBvblRhcCgpIHsKICAgIG15Q29tcG9uZW50LnNwZWVkLnggPSAtNSArIDEwICogX3JuZy5uZXh0RG91YmxlKCk7CiAgICBteUNvbXBvbmVudC5zcGVlZC55ID0gLTUgKyAxMCAqIF9ybmcubmV4dERvdWJsZSgpOwogIH0KfQo=",
+      "type": "text"
+    },
+    {
+      "path": "pubspec.yaml",
+      "data":
+          "bmFtZToge3tuYW1lfX0KZGVzY3JpcHRpb246IHt7ZGVzY3JpcHRpb259fQp2ZXJzaW9uOiB7e3ZlcnNpb259fQoKcHVibGlzaF90bzogJ25vbmUnCgplbnZpcm9ubWVudDoKICBzZGs6ICI+PTIuMTcuMCA8My4wLjAiCgpkZXBlbmRlbmNpZXM6CiAgZmx1dHRlcjoKICAgIHNkazogZmx1dHRlcnt7I2V4dHJhLWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGVwZW5kZW5jaWVzfX0KCmRldl9kZXBlbmRlbmNpZXM6e3sjZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAge3tuYW1lfX06IHt7dmVyc2lvbn19e3svZXh0cmEtZGV2LWRlcGVuZGVuY2llc319CiAgZmx1dHRlcl90ZXN0OgogICAgc2RrOiBmbHV0dGVyCiAgaW50ZWdyYXRpb25fdGVzdDoKICAgIHNkazogZmx1dHRlcgoKZmx1dHRlcjoKICB1c2VzLW1hdGVyaWFsLWRlc2lnbjogZmFsc2U=",
+      "type": "text"
+    },
+    {
+      "path": "test/widget_test.dart",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOmZsYW1lL2dhbWUuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbGFtZV90ZXN0L2ZsYW1lX3Rlc3QuZGFydCc7CmltcG9ydCAncGFja2FnZTpmbHV0dGVyX3Rlc3QvZmx1dHRlcl90ZXN0LmRhcnQnOwoKaW1wb3J0ICdwYWNrYWdlOnt7bmFtZX19L21haW4uZGFydCc7CgpmaW5hbCBteUdhbWUgPSBGbGFtZVRlc3RlcihNeUdhbWUubmV3KTsKCnZvaWQgbWFpbigpIHsKICBteUdhbWUudGVzdEdhbWVXaWRnZXQoCiAgICAnZ2FtZSB3aWxsIGxvYWQgaXRzIGNoaWxkJywKICAgIHZlcmlmeTogKGdhbWUsIHRlc3RlcikgYXN5bmMgewogICAgICBnYW1lLnVwZGF0ZSgwLjApOwoKICAgICAgZXhwZWN0KGdhbWUuY2hpbGRyZW4ubGVuZ3RoLCAxKTsKICAgICAgZXhwZWN0KGdhbWUubXlDb21wb25lbnQuc3BlZWQsIFZlY3RvcjIuemVybygpKTsKCiAgICAgIGF3YWl0IHRlc3Rlci50YXBBdChjb25zdCBPZmZzZXQoMTAsIDEwKSk7CiAgICAgIGV4cGVjdChnYW1lLm15Q29tcG9uZW50LnNwZWVkLCBpc05vdChlcXVhbHMoVmVjdG9yMi56ZXJvKCkpKSk7CiAgICB9LAogICk7Cn0K",
+      "type": "text"
+    }
+  ],
+  "hooks": [],
+  "name": "example",
+  "description":
+      "An actual complete, working game example. Extra code that you won't need but will teach you how to wire the most important pieces.",
+  "version": "0.1.0",
+  "environment": {"mason": "any"},
+  "vars": {
+    "name": {"type": "string"},
+    "description": {"type": "string"},
+    "version": {"type": "string"},
+    "flame-version": {"type": "string"},
+    "flame-lint-version": {"type": "string"},
+    "extra-dependencies": {"type": "array"},
+    "extra-dev-dependencies": {"type": "array"}
+  }
+});
