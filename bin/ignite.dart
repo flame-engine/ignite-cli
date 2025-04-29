323759326c6a34eac8da592d8a5498fe39859d15
Elijah Luckey <luckeyelijah112@gmail.com>
2025-04-28 23:00:05 -0400

    fix closing buffer for stdout and stdin

--- bin/ignite.dart
+++ bin/ignite.dart
@@ -6,12 +6,11 @@ import 'package:ignite_cli/ignite_context.dart';
 import 'package:mason_logger/mason_logger.dart';
 
 Future<void> main(List<String> args) async {
-  final runner = IgniteCommandRunner(
-    IgniteContext(
-      logger: Logger(),
-      flameVersionManager: await FlameVersionManager.fetch(),
-    ),
-  );
+  final manager = await FlameVersionManager.fetch();
+  final context = IgniteContext(logger: Logger(), flameVersionManager: manager);
+  final runner = IgniteCommandRunner(context);
+  final code = await runner.run(args);
 
-  exit(await runner.run(args));
+  // ensure all buffered output is written before exit
+  await Future.wait([stdout.close(), stderr.close()]).then((_) => exit(code));
 }
